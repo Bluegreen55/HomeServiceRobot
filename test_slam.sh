0b78b2878452246cf25f635c76296efd037af1cf
Bluegreen55 <33987636+Bluegreen55@users.noreply.github.com>
2021-02-04 04:10:06 +0000

    first version of .sh files

---
+++ test_slam.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#xterm -e " source /opt/kinetic/setup.bash; roscore " &
+
+#sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/my_robot/worlds/jenna2.world" &
+
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
+sleep 5
+
+xterm -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py" 
+ 
+#sleep 5
